a4f493f8b727e25b1fccec91f937cc245f0257c0
Ferckman <djferckman@gmail.com>
2025-07-23 17:36:51 +0200

    21

--- js/uiManager.js
+++ js/uiManager.js
@@ -1,6 +1,6 @@
 export function initStartButton(callback) {
   const btn = document.createElement("button");
-  btn.textContent = "Encender núcleo de control20";
+  btn.textContent = "Encender núcleo de control21";
   btn.className = "start-btn";
   document.body.appendChild(btn);
 
